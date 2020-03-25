eb4a777fcd2a5aa75d7420aa842f3eb0df2c1434
Pavro <pablomillancb@gmail.com>
2020-03-25 17:18:23 +0100

    Imagenes v3

--- nabot.py
+++ nabot.py
@@ -234,7 +234,7 @@ def restart(update,context):
         update.message.reply_text("Permiso denegado.")
 
 def stonk(update, context):
-    update.from_user.send_photo('https://pbs.twimg.com/media/ETyCZvJU8AAm8LN?format=jpg&name=900x900')
+    update.message.from_user.send_photo('https://pbs.twimg.com/media/ETyCZvJU8AAm8LN?format=jpg&name=900x900')
 
 
 def main():
